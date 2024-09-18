b8c7d4693efea126b6cc2086ba7999012b896eda
Sirichai <mercier_1999@hotmail.com>
2024-09-18 10:19:53 +0700

    set up config and initial

---
+++ lib/core/configs/theme/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff42C83C);
+  static const lightBackground = Color(0xffF2F2F2);
+  static const darkBackground = Color(0xff1A1A1A);
+  static const gray = Color(0xffBEBEBE);
+  static const darkGray = Color(0xff343434);
+}
